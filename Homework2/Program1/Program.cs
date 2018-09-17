fe0466480977ddf913936f18efa1420d9f686d1a
SueWinky <944738048@qq.com>
2018-09-17 21:11:00 +0800

    第二次提交

---
+++ Homework2/Program1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= n - 1; i++)
+            {
+                if (n % i == 0)
+                {
+                    bool right = true;
+                    for (int j = 2; j <= Math.Sqrt(i); j++)
+                    {
+                        if(i%j == 0)
+                        {
+                            right = false;
+                        }
+                    }
+                    if (right)
+                    {
+                        Console.WriteLine(i.ToString());
+                    }
+
+                }
+            }
+            Console.ReadLine();
+        }    
+    }
+}
---
+++ Homework2/Program3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(2 + " ");
+            for (int m = 3; m <= 100; m += 2)
+            {
+                bool a = true;
+                if (m % 2 == 0)
+                {
+                    a = false;
+                }
+                else
+                {
+                    for (int i = 3; i < m / 2; i += 2)
+                    {
+                        if (m % i == 0)
+                        {
+                            a = false;
+                            break;
+                        }
+                    }
+                }
+                if (a == true)
+                {
+                    Console.Write(m.ToString() + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
