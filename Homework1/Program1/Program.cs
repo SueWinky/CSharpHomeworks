bd8e7ad030043e2466c3ae126918d01a5d710668
SueWinky <944738048@qq.com>
2018-09-17 21:37:21 +0800

    第四次提交

---
+++ Homework1/Program1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("输入第一个数：");
+            string d1 = Console.ReadLine();
+            Console.Write("输入第二个数：");
+            string d2 = Console.ReadLine();
+            Console.WriteLine("两个个数的乘积是:{0}", Convert.ToInt32(d1) * Convert.ToInt32(d2));
+        }
+    }
+
+}
---
+++ Homework1/Program2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+        int a, b;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32(textBox1.Text.ToString());
+        }
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+            b = Convert.ToInt32(textBox2.Text.ToString());
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            label1.Text = "两数之积为：" + (a * b).ToString();
+        }
+    }
+}
